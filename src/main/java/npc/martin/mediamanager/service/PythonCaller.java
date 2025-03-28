8bdf35163291832446ec695cfa54632e407504f0
bikathi-martin <bikathimartin@outlook.com>
2025-03-28 11:03:28 +0300

    Modify the python process calling method to only pass watermark if provided

--- src/main/java/npc/martin/mediamanager/service/PythonCaller.java
+++ src/main/java/npc/martin/mediamanager/service/PythonCaller.java
@@ -3,25 +3,21 @@ package npc.martin.mediamanager.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URL;
+import java.util.Objects;
 
 @Service
 public class PythonCaller {
-
     private static final Logger logger = LoggerFactory.getLogger(PythonCaller.class);
 
     public final void callPythonScript(String inputFilePath, String outputFilePath, String watermarkText) {
-        logger.info("Calling python script");
         try {
-//            URL scriptUrl = getClass().getClassLoader().getResource("compressor.py");
-
             // Execute the Python script
-            ProcessBuilder processBuilder = new ProcessBuilder("python3", "/app/compressor.py", inputFilePath, outputFilePath, watermarkText);
-            processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
+            Process process = getProcess(inputFilePath, outputFilePath, watermarkText);
             try (BufferedReader processOutputReader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
                 String line;
                 while ((line = processOutputReader.readLine()) != null) {
@@ -37,5 +33,24 @@ public class PythonCaller {
         } catch (Exception e) {
             logger.error("An error occurred while executing the Python script.", e);
         }
+    }
+
+    private Process getProcess(String inputFilePath, String outputFilePath, String watermarkText) throws IOException {
+        ProcessBuilder processBuilder = Objects.isNull(watermarkText) || !StringUtils.hasLength(watermarkText) ? new ProcessBuilder(
+            "python3",
+            "/app/compressor.py",
+            inputFilePath,
+            outputFilePath
+        ) : new ProcessBuilder(
+            "python3",
+            "/app/compressor.py",
+            inputFilePath,
+            outputFilePath,
+            "--watermark_text",
+            watermarkText
+        );
+
+        processBuilder.redirectErrorStream(true);
+        return processBuilder.start();
     }
 }
